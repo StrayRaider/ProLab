63c7298ce5ac2fc05150350c3fbb1dee4f659fa7
StrayRaider <41emrekaya919@gmail.com>
2022-10-05 14:06:45 +0300

    eşitleme  denemesi

--- Dokuwiki/code/file_dir.h
+++
@@ -1,1 +0,0 @@
-void find_file(char *path,char files[50][50],int *file_count);
--- Dokuwiki/code/read_f.h
+++
@@ -1,5 +0,0 @@
-// .txt formatında dosyayı okur verilen file_cont değişkenine yazar
-char ** read_file(char* file_name,char link_list[50][50]);
-
-//dosya adına .txt uzantısı ekler
-void add_txt(char*file_name);
